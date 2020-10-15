c3c843b429521a0eb03928c6cd07b782eb92dcab
Abdullah0095 <abdullah.web1995@gmail.com>
2020-10-15 12:34:04 +0600

    nice one

--- src/components/CustomerZone/Order/Order.js
+++ src/components/CustomerZone/Order/Order.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Order = () => {
     return (
-        <div>
-            <h1>Order</h1>
+        <div className="row">
+
+            <div className="col-md-2 container-fluid justify-content-left" >
+                <Sidebar />
+            </div>
+
+            <div className="col-md-8 container-fluid" style={{ margin: '30px 0px 0px 40px' }}>
+                <div>
+                    <h2>Order</h2>
+                </div>
+
+
+
+                <div style={{ marginTop: '40px' }}>
+                    <form>
+                        <div class="form-group">
+
+                            <input type="name" class="form-control" placeholder="Your name/ Company's name" />
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <input type="email" class="form-control" placeholder="Your email" />
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <input type="name" class="form-control" placeholder=" " />
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <input type="name" class="form-control" placeholder=" " />
+
+                        </div>
+
+                        <div class="form-group">
+
+                            <input type="number" class="form-control" placeholder="Price" />
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary">Send us</button>
+                    </form>
+                </div>
+            </div>
+
+
+
         </div>
     );
 };
--- src/components/CustomerZone/Sidebar/Sidebar.js
+++ src/components/CustomerZone/Sidebar/Sidebar.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logos/logo.png';
 
 const Sidebar = () => {
     return (
-        <div>
-            <h1>sidebar</h1>
-        </div>
+        <section style={{width: '1440px', height: '900px'}}>
+
+            <div style={{margin: '30px 0px 0px 40px'}}>
+                <Link to="/home">
+                    <img style={{ width: '150px', height: '47px' }} src={logo} alt="" />
+                </Link>
+            </div>
+
+            <div style={{marginTop: '60px', marginLeft: '70px'}}>
+                <Link style={{textDecoration:'none'}}>
+                    <h5>Order</h5><br/>
+                </Link>
+
+                <Link style={{textDecoration:'none'}}>
+                    <h5>Service List</h5><br/>
+                </Link>
+
+                <Link style={{textDecoration:'none'}}>
+                    <h5>Review</h5>
+                </Link>
+            </div>
+        </section>
     );
 };
 
